531eab1e6669ffa51e7352961f5c5421335d7eca
AzareelJames <azareeljameslbasalo@gmail.com>
2025-08-03 15:35:23 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const webview = document.createElement(`iframe`);
+webview.src = `https://azareeljames.github.io/console/`;
+webview.classList = `webview`;
+var input;
+document.body.appendChild(webview);
+
+const search = document.createElement(`button`);
+search.classList = `search`;
+search.title = `search`;
+search.textContent = `Search`; 
+
+function Search(){
+    if (confirm(`Search or type URL? (click "no" if you want to type URL)`)){
+        input = 
+        prompt(`Search (Leave a blank to cancel or type "\\b" to back or type "\\r" to redo)`);
+        if (input === `\\b`){
+            try{
+                webview.contentWindow.history.back();
+                return
+            }
+            catch{
+                alert(`Cannot back from the page`);
+                return
+            }
+        }
+        else if (input === `\\r`){
+            try{
+                webview.contentWindow.history.forward();
+                return
+            }
+            catch{
+                alert(`Cannot redo from the page`);
+                return
+            }
+        }
+        if (input){
+            webview.src = `https://www.bing.com/search?q=${input}`;
+        }
+    }
+    else{
+        input =
+        prompt(`Search (Leave a blank to cancel or make sure there's with https://)`);
+        if (input){
+            webview.src = input;
+        }
+    }
+}
+
+search.onclick = Search;
+document.body.appendChild(search);
